cdeb1609c713a51f2c07eed79ca5a6ba8a0baebb
unknown <leonardo.norambuenam@gmail.com>
2020-06-18 16:33:35 -0400

    add description to manifest

--- venta_movil/__manifest__.py
+++ venta_movil/__manifest__.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "Venta Móvil JP",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Aplicación de venta móvil 
+        
+        """,
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Dimabe Ltda",
+    'website': "http://www.dimabe.cl",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
