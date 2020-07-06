209fe87d1091e000d7263a36cd524d373e9785a5
unknown <leonardo.norambuenam@gmail.com>
2020-07-06 18:30:15 -0400

    return request user

--- venta_movil/controllers/ProfileController.py
+++ venta_movil/controllers/ProfileController.py
@@ -7,8 +7,5 @@ class ProfileController(http.Controller):
 
     @http.route('/api/user', type='json', auth='token', cors='*')
     def get_user(self):
-        u_email = request.get('uemail')
-        if (u_email):
-            #user = request.env['res.partner'].search_read([('email', '=', request.uemail), ['name', 'email', 'phone'])
-            return {'user': 'llegó acá'}
-        return {'bad': u_email}
+        data = json.loads(request.httprequest.data)
+        return {'bad': data}
