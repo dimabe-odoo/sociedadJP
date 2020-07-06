030d0d6853b9796e52663ac87439de1369e6a3b6
unknown <leonardo.norambuenam@gmail.com>
2020-07-06 18:32:23 -0400

    return request user

--- venta_movil/controllers/ProfileController.py
+++ venta_movil/controllers/ProfileController.py
@@ -1,11 +1,10 @@
 from odoo import http
 from odoo.http import request
-from ..jwt_token import generate_token
 import werkzeug
 
 class ProfileController(http.Controller):
 
     @http.route('/api/user', type='json', auth='token', cors='*')
-    def get_user(self):
+    def get_user(self, **Kw):
         data = json.loads(request.httprequest.data)
         return {'bad': data}
