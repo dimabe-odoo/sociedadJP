8a1e2a0ae04a8330e3549d06d787fbc7005b3f2f
unknown <leonardo.norambuenam@gmail.com>
2020-07-06 16:49:38 -0400

    change email for login validation

--- venta_movil/controllers/RegisterController.py
+++ venta_movil/controllers/RegisterController.py
@@ -6,12 +6,12 @@ class LoginController(http.Controller):
     @http.route('/api/register', type='json', auth='public', cors='*')
     def do_register(self, name, password, email, phoneNumber):
         # search user exist
-        user = request.env['res.users'].sudo().search([('email', '=', email)])
+        user = request.env['res.users'].sudo().search([('login', '=', email)])
 
         if user:
             return self.errcode(code=400, message='Usuario registrado con anterioridad')
 
-        contact = request.env['res.partner'].sudo().search([('login', '=', email)])
+        contact = request.env['res.partner'].sudo().search([('email', '=', email)])
 
         if contact: 
             contact.write({'name': name, 'email': email, 'mobile': phoneNumber})
