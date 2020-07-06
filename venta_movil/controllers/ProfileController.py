8bc321ed46853938f2e3a69ecb834d102dbae1cd
unknown <leonardo.norambuenam@gmail.com>
2020-07-06 18:56:08 -0400

    return request user

--- venta_movil/controllers/ProfileController.py
+++ venta_movil/controllers/ProfileController.py
@@ -5,5 +5,6 @@ import werkzeug
 class ProfileController(http.Controller):
 
     @http.route('/api/user', type='json', methods=['GET'], auth='token', cors='*')
-    def do_user(self, uemail):
-        return {'bad': uemail}
+    def do_user(self):
+
+        return {'bad': request.uid}
