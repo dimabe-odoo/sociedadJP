5c8a54a6a5c33e84770e044d6f3896492c25e8ec
unknown <leonardo.norambuenam@gmail.com>
2020-07-06 17:39:24 -0400

    add token method to controllers

---
+++ venta_movil/controllers/ProfileController.py
@@ -0,0 +1,10 @@
+from odoo import http
+from odoo.http import request
+from ..jwt_token import generate_token
+import werkzeug
+
+class ProfileController(http.Controller):
+
+    @http.route('/api/user', type='json', auth='token', cors='*')
+    def get_user(self):
+        return {'user': request.user}
