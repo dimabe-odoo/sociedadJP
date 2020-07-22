eb471c3415489e05801664231660c175e36a9a13
Leonardo Norambuena <leonardonorambuena@MacBook-Pro-de-Leonardo.local>
2020-07-22 12:11:24 -0400

    change response in communecontroller

--- venta_movil/controllers/CommuneController.py
+++ venta_movil/controllers/CommuneController.py
@@ -7,4 +7,4 @@ class CommuneController(http.Controller):
     @http.route('/api/communes', type='json', methods=['GET'], auth='token', cors='*')
     def do_user(self):
         communes = request.env['jp.commune'].sudo().search_read([('state_id', '=', 1176)], ['name'])
-        return {'communes': communes}
+        return communes
