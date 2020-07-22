f44b65ce3b40e235d7154eea4fcdbbfce9321b45
Leonardo Norambuena <leonardonorambuena@MacBook-Pro-de-Leonardo.local>
2020-07-22 11:17:57 -0400

    fix error in partner model

--- venta_movil/models/res_partner.py
+++ venta_movil/models/res_partner.py
@@ -2,4 +2,4 @@ from odoo import models, fields
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    jp_commune_id = fields.Many2one('jp.commune', string='Comuna'
+    jp_commune_id = fields.Many2one('jp.commune', string='Comuna')
