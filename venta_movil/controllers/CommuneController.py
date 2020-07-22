090cc33f0c267cf4320997ee33bd354d9c5d90e3
Leonardo Norambuena <leonardonorambuena@MacBook-Pro-de-Leonardo.local>
2020-07-22 12:28:32 -0400

    remove search_read for search

--- venta_movil/controllers/CommuneController.py
+++ venta_movil/controllers/CommuneController.py
@@ -6,7 +6,7 @@ class CommuneController(http.Controller):
 
     @http.route('/api/communes', type='json', methods=['GET'], auth='token', cors='*')
     def do_user(self):
-        communes = request.env['jp.commune'].sudo().search_read([('state_id', '=', 1176)], ['name'])
+        communes = request.env['jp.commune'].sudo().search([('state_id', '=', 1176)])
         result = []
         for res in communes:
             result.append({'name': res.name})
