cb7f8847a66115abe4bb020899bd81bbd47febcf
Leonardo Norambuena <leonardonorambuena@MacBook-Pro-de-Leonardo.local>
2020-07-24 16:57:17 -0400

    order by name communes

--- venta_movil/controllers/CommuneController.py
+++ venta_movil/controllers/CommuneController.py
@@ -6,7 +6,7 @@ class CommuneController(http.Controller):
 
     @http.route('/api/communes', type='json', methods=['GET', 'POST'], auth='token', cors='*')
     def get_communes(self):
-        communes = request.env['jp.commune'].sudo().search([('state_id', '=', 1176)])
+        communes = request.env['jp.commune'].sudo().search([('state_id', '=', 1176)], order='name asc')
         result = []
         for res in communes:
             result.append({
