5bdb484a5d3663a93dff2c42a3626b83d73c117c
Leonardo Norambuena <leonardonorambuena@MacBook-Pro-de-Leonardo.local>
2020-07-24 16:08:09 -0400

    fix controller

--- venta_movil/controllers/AddressController.py
+++ venta_movil/controllers/AddressController.py
@@ -8,10 +8,11 @@ class AddressController(http.Controller):
     @http.route('/api/addresses', type='json', methods=['POST'], auth='token', cors='*')
     def get_communes(self, commune_id, city, address, latitude, longitude):
         contact = request.env['res.partner'].sudo().search(['user_id', '=', request.uid])
-        raise werkzeug.exceptions.BadRequest(contact)
+
         if not contact:
             raise werkzeug.exceptions.BadRequest('Imposible encontrar el contacto asociado al usuario')
-        contact.write({'state_id': 1176,'jp_commune_id': commune_id, 'city': city, 'street': address, 'partnet_latitude': latitude, 'partner_longitude': longitude, 'date_localization': date.today()})
+        
+        contact.update({'state_id': 1176,'jp_commune_id': commune_id, 'city': city, 'street': address, 'partnet_latitude': latitude, 'partner_longitude': longitude})
 
         return {'message': 'Dirección creada correctamente'}
         
