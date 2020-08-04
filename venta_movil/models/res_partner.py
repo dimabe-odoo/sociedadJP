91c1194c390d2a24e1946fa340a3a76cea5f0650
Leonardo Norambuena <leonardonorambuena@MacBook-Pro-de-Leonardo.local>
2020-08-04 18:05:46 -0400

    Required fields, default CL Country

--- venta_movil/models/res_partner.py
+++ venta_movil/models/res_partner.py
@@ -3,3 +3,9 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     jp_commune_id = fields.Many2one('jp.commune', string='Comuna')
+    country_id = fields.Many2one('res.country', string='País',default=_get_default_country)
+
+    @api.model
+    def _get_default_country(self):
+        country = self.env['res.country'].search([('code', '=', 'CL')], limit=1)
+        return country
