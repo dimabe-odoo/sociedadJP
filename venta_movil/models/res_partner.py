63b670204866cfae0b58204df9591d5b0f8149de
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-04 18:22:27 -0400

    modify default country

--- venta_movil/models/res_partner.py
+++ venta_movil/models/res_partner.py
@@ -1,11 +1,8 @@
 from odoo import models, fields, api
 
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     jp_commune_id = fields.Many2one('jp.commune', string='Comuna')
-    country_id = fields.Many2one('res.country', string='País',default=_get_default_country())
-
-    @api.model
-    def _get_default_country(self):
-        country = self.env['res.country'].search([('code', '=', 'CL')], limit=1)
-        return country
+    country_id = fields.Many2one('res.country', string='País', default=lambda self: self.env[
+        'res.country'].search([('code', '=', 'CL')]))
