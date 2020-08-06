033b1df71c69e4d68b33dabd5b34f9b1f5ea7e82
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-06 15:51:27 -0400

    fix error in product view

--- venta_movil/models/product_product.py
+++ venta_movil/models/product_product.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
+
     supply_id = fields.Many2one('product.product', string='Insumo')
