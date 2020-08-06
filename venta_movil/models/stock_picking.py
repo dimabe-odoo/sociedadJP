29a7d0ea014193b774ffc63a26f63576963c5800
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2020-08-06 17:53:52 -0400

    debug stock_picking

--- venta_movil/models/stock_picking.py
+++ venta_movil/models/stock_picking.py
@@ -4,11 +4,11 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
 
-def button_validate(self):
-    for stock_picking in self:
-        raise models.ValidationError(stock_picking)
-        message = ''
-        product = self.env['product.product'].browse(stock_picking.product_ids[0].id)
-        available_qty = product.with_context({'warehouse' : stock_picking.warehouse_id}).qty_available
-        if stock_picking.quantity_done < 
+    def button_validate(self):
+        for stock_picking in self:
+            raise models.ValidationError(stock_picking)
+            message = ''
+            product = self.env['product.product'].browse(stock_picking.product_ids[0].id)
+            available_qty = product.with_context({'warehouse' : stock_picking.warehouse_id}).qty_available
+            #if stock_picking.quantity_done < 
 
