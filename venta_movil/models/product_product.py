75a4cc14f0597559ef8241e2310d614f049eaa84
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2020-08-06 15:33:26 -0400

    add supply id field

--- venta_movil/models/product_product.py
+++ venta_movil/models/product_product.py
@@ -1,4 +1,4 @@
 from odoo import models, fields, api
 class ProductProduct(models.Model):
     _inherit = 'product.product'
-    supply_id = Many2one('product.product', string='Insumo')
+    supply_id = fields.Many2one('product.product', string='Insumo')
