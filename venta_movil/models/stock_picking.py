421d79bc3f4caa056a839e82b8dc292e74413dae
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2020-08-07 10:54:10 -0400

    debug validate stock picking

--- venta_movil/models/stock_picking.py
+++ venta_movil/models/stock_picking.py
@@ -7,7 +7,7 @@ class StockPicking(models.Model):
     def button_validate(self):
         for stock_picking in self:
             message = ''
-            supply = self.env['product.product'].browse(stock_picking.product_ids[0].supply_id)
+            supply = self.env['product.product'].browse(stock_picking.move_ids_without_package[0].supply_id)
             available_qty = supply.with_context({'warehouse' : stock_picking.warehouse_id}).qty_available
             raise models.ValidationError(available_qty)
             #if stock_picking.quantity_done < 
