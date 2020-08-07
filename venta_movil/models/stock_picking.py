d7d3253b3aa7f6fef0c188999cf77d561685eaf6
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-07 11:00:14 -0400

    debug stock_quant of supply

--- venta_movil/models/stock_picking.py
+++ venta_movil/models/stock_picking.py
@@ -1,14 +1,16 @@
 from odoo import models, fields
 
+
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
-
 
     def button_validate(self):
         for stock_picking in self:
             message = ''
-            supply = self.env['product.product'].browse(stock_picking.move_ids_without_package[0].supply_id)
-            available_qty = supply.with_context({'warehouse' : stock_picking.warehouse_id}).qty_available
-            raise models.ValidationError(available_qty)
-            #if stock_picking.quantity_done < 
-
+            for move in stock_picking.move_ids_without_package:
+                if move.product_id.supply_id:
+                    supply_id = move.product_id.supply_id.id
+                    quant = self.env['stock.quant'].search(
+                        [('product_id', '=', supply_id), ('location_id', '=', stock_picking.location_id)])
+                    raise models.ValidationError(quant.quantity)
+                # if stock_picking.quantity_done <
