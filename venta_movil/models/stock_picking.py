cc4f2012da3092c2e643031bef6ff905f9644ca2
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-07 12:05:06 -0400

    modify button_validate of stock_picking

--- venta_movil/models/stock_picking.py
+++ venta_movil/models/stock_picking.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+import datetime
 
 
 class StockPicking(models.Model):
@@ -7,12 +8,69 @@ class StockPicking(models.Model):
     def button_validate(self):
         for stock_picking in self:
             message = ''
+            stock_moves = []
+
             for move in stock_picking.move_ids_without_package:
                 if move.product_id.supply_id:
-                    supply_id = move.product_id.supply_id.id
+                    supply_id = move.product_id.supply_id
                     quant = self.env['stock.quant'].search(
-                        [('product_id', '=', supply_id), ('location_id', '=', stock_picking.location_dest_id.id)])
-                    raise models.ValidationError(quant.quantity)
+                        [('product_id', '=', supply_id.id), ('location_id', '=', stock_picking.location_dest_id.id)])
+                    if quant.quantity < move.product_uom_qty:
+                        raise models.UserError('No tiene la cantidad necesaria de insumos {}'.format(
+                            supply_id.display_name))
+                    stock_move = stock_moves.append({
+                        'company_id': self.env.user.company_id.id,
+                        'date': datetime.datetime.now(),
+                        'location_id': stock_picking.location_dest_id.id,
+                        'product_id': supply_id.id,
+                        'product_uom': supply_id.uom_id,
+                        'product_uom_qty': move.product_uom_qty
+                    })
                 # if stock_picking.quantity_done <
-                res = super(StockPicking, self).button_validate()
-                return res
+
+            res = super(StockPicking, self).button_validate()
+            if res:
+                if stock_picking.picking_type_code == 'out_going':
+                    name = 'IN/{}'.format(stock_picking.name)
+                    dispatch = self.env['stock.picking'].create({
+                        'name': name,
+                        'picking_type_code': 'in_coming',
+                        'origin': stock_picking.origin,
+                        'state':'done',
+                        'partner_id': stock_picking.partner_id.id
+                    })
+                    location_dest = self.env['stock.location'].search([('name', '=', 'Customers')])
+                else:
+                    name = 'OUT/{}'.format(stock_picking.name)
+                    dispatch = self.env['stock.picking'].create({
+                        'name': name,
+                        'picking_type_code': 'out_going',
+                        'origin': stock_picking.origin,
+                        'state': 'done',
+                        'partner_id': stock_picking.partner_id.id
+                    })
+                    location_dest = self.env['stock.location'].search([('name', '=', 'Vendors')])
+                for stock in stock_moves:
+                    move = self.env['stock.move'].create({
+                        'picking_id': dispatch.id,
+                        'company_id': stock.company_id,
+                        'date': stock.date,
+                        'location_id': stock.location_id,
+                        'location_dest_id': location_dest.id,
+                        'state': 'done',
+                        'product_id': stock.product_id,
+                        'product_uom': stock.product_uom,
+                        'product_uom_qty': stock.product_uom_qty
+                    })
+                    self.env['stock.move.line'].create({
+                        'move_id': move.id,
+                        'company_id': move.company_id.id,
+                        'date': move.date,
+                        'state': 'done',
+                        'location_id': move.location_id.id,
+                        'product_uom_id': move.product_uom.id,
+                        'product_uom_qty': move.product_uom_qty,
+                        'qty_done':move.product_uom_qty,
+                        'location_dest_id': move.location_dest_id.id,
+                    })
+            return res
