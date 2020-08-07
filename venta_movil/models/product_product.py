dd19dbeb6b2b4f617c1e4444e78903631a610954
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2020-08-07 16:22:34 -0400

    change supply name

--- venta_movil/models/product_product.py
+++ venta_movil/models/product_product.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    supply_id = fields.Many2one('product.product', string='Insumo')
+    supply_id = fields.Many2one('product.product', string='Envase')
