a43938e7de5f4d870c0759ad051de886c5c21468
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-24 14:07:54 -0400

    modify action_confirm of sale_order for reception of supply

--- venta_movil/models/sale_order.py
+++ venta_movil/models/sale_order.py
@@ -1,12 +1,53 @@
 from odoo import fields, models, api
+from datetime import datetime
 
 
-class ModelName (models.Model):
+class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     loan_supply = fields.Boolean('Prestamo de Cilindro')
 
+    supply_reception_id = fields.Many2one('Entrado de insumo')
 
-    
-
-
+    def action_confirm(self):
+        self.ensure_one()
+        if not self.loan_supply:
+            for line in self.order_line:
+                if line.product_id.supply_id:
+                    stock_picking = self.env['stock.picking'].create({
+                        'parent_id': self.parent_id.id,
+                        'location_id': self.env['stock.location'].search([('name', '=', 'Customers')]).id,
+                        'location_dest_id': self.warehouse_id.lot_stock_id.id,
+                        'picking_type_id': self.env['stock.picking.id'].search(
+                            [('sequence_code', '=', 'IN'), ('warehouse_id', '=', self.warehouse_id.id)]).id,
+                        'origin': self.name,
+                        'company_id': self.env.user.company_id.id
+                    })
+                    stock_move = self.env['stock_move'].create({
+                        'name': 'SUPPLY/' + stock_picking.name,
+                        'picking_id': stock_picking.id,
+                        'company_id': self.env.user.company_id.id,
+                        'date': datetime.utcnow(),
+                        'location_id': self.env['stock.location'].search([('name', '=', 'Customers')]).id,
+                        'location_dest_id': self.warehouse_id.lot_stock_id.id,
+                        'state': 'progress',
+                        'product_id': line.product_id.id,
+                        'product_uom': line.product_uom.id,
+                        'product_uom_qty': line.product_uom_qty
+                    })
+                    self.env['stock.move.line'].create({
+                        'move_line': stock_move.id,
+                        'picking_id': stock_picking.id,
+                        'company_id': self.env.user.company_id.id,
+                        'date': datetime.utcnow(),
+                        'location_id': self.env['stock.location'].search([('name', '=', 'Customers')]).id,
+                        'location_dest_id': self.warehouse_id.lot_stock_id.id,
+                        'product_id': line.product_id.id,
+                        'qty_done': line.product_uom_qty,
+                        'product_uom': line.product_uom.id,
+                    })
+                    stock_picking.button_validate()
+                    self.write({
+                        'supply_reception_id': stock_picking.id
+                    })
+            super(SaleOrder, self).action_confirm()
