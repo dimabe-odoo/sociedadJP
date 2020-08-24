f3113ebaf9cbe8b056b4679899be16b1b276a4ab
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-24 12:00:12 -0400

    add validation to stock_picking

--- venta_movil/__manifest__.py
+++ venta_movil/__manifest__.py
@@ -31,8 +31,8 @@
         'views/product_product.xml',
         'views/templates.xml',
         'views/stock_picking.xml'
-        # 'views/sale_order.xml',
-        # 'views/stock_location.xml'
+        'views/sale_order.xml',
+        'views/stock_location.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- venta_movil/models/__init__.py
+++ venta_movil/models/__init__.py
@@ -11,3 +11,7 @@ from . import res_partner
 from . import product_product
 
 from . import stock_picking
+
+from . import stock_location
+
+from . import sale_order
