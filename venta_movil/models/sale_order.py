cda7470336cd21cd20e68d5fc3955faeed507d04
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-25 13:39:48 -0400

    add misssing res

--- venta_movil/models/sale_order.py
+++ venta_movil/models/sale_order.py
@@ -6,15 +6,3 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     loan_supply = fields.Boolean('¿Es prestamo de cilindro?')
-
-    supply_reception_id = fields.Many2one('stock.picking', 'Entrada de insumo')
-
-    def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
-        if self.loan_supply:
-            for loan in self.picking_ids:
-                loan.write({
-                    'loan_supply': True,
-                    'location_dest_id': self.warehouse_id.location_dest_id.id
-                })
-        return res
