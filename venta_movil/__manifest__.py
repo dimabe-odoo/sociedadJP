b24cdab521485ad42654eba5fe49f636fc849ab6
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-26 08:55:26 -0400

    test

--- venta_movil/__manifest__.py
+++ venta_movil/__manifest__.py
@@ -33,7 +33,8 @@
         'views/stock_picking.xml',
         'views/sale_order.xml',
         'views/stock_location.xml',
-        'views/stock_warehouse.xml'
+        'views/stock_warehouse.xml',
+        'static/xml/template.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
