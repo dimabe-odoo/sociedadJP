2e39b9017fbb0041281c2da542d851cffc5b1607
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-27 16:02:22 -0400

    test

--- venta_movil/models/purchase_order_line.py
+++ venta_movil/models/purchase_order_line.py
@@ -6,4 +6,8 @@ class PurchaseOrderLine(models.Model):
 
     purchase_without_supply = fields.Integer('Compra comodato')
 
-
+    @api.onchange('purchase_without_supply')
+    def onchange_purchase_without_supply(self):
+        if self.product_qty < self.purchase_without_supply:
+            raise models.ValidationError(
+                'La cantidad de de compra comodato no puede ser mayor a la cantidad solicitidad')
