435369f7246694ced4e6943459f5539791e9383f
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-27 13:35:14 -0400

    add field purchase_without_supply to purchase_order

--- venta_movil/models/purchase_order_line.py
+++ venta_movil/models/purchase_order_line.py
@@ -5,3 +5,5 @@ class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     purchase_without_supply = fields.Integer('Compra comodato')
+
+    have_purchase_without_supply = fields.Boolean('¿Tiene compra comodato?')
