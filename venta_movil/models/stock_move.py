5d2b3c93a4f02f8b89fa055071de4addc900f9f5
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-27 16:59:13 -0400

    add field purchase_without_supply to purchase_order

--- venta_movil/models/stock_move.py
+++ venta_movil/models/stock_move.py
@@ -1,15 +1,13 @@
 from odoo import fields, models, api
+from odoo.addons import decimal_precision as dp
 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    purchase_without_supply = fields.Float('Comprar comodato')
+    purchase_without_supply = fields.Float('Comprar comodato', digits=dp.get_precision('Product Unit of Measure'))
 
     @api.onchange
     def on_change_qty(self):
         if self.product_uom_qt < self.purchase_without_supply:
             raise models.ValidationError('Error')
-    
-
-
