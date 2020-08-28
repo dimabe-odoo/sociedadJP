d81df970496431428c3a9caaaaad7ca7c11e0040
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-28 11:45:05 -0400

    modify stock_picking button validate

--- venta_movil/models/stock_move.py
+++ venta_movil/models/stock_move.py
@@ -7,7 +7,14 @@ class StockMove(models.Model):
 
     purchase_without_supply = fields.Float('Comprar comodato', digits=dp.get_precision('Product Unit of Measure'))
 
-    @api.onchange
+    loan_supply = fields.Float('Prestamo',digits=dp.get_precision('Product Unit of Measure'))
+
+    @api.onchange('purchase_without_supply')
     def on_change_qty(self):
-        if self.product_uom_qt < self.purchase_without_supply:
-            raise models.ValidationError('Error')
+        if self.product_uom_qty < self.purchase_without_supply:
+            raise models.ValidationError('La compra comodato no puede ser mayor a la demanda')
+
+    @api.onchange('loan_supply')
+    def on_change_qty(self):
+        if self.product_uom_qty < self.loan_supply:
+            raise models.ValidationError('El prestamo no puede ser mayor a la demanda')
