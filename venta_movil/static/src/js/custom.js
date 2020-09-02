b45b429b3e141f37c93101974ecfc7a513e96e20
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-02 17:05:15 -0400

    change template in custom.js

--- venta_movil/static/src/js/custom.js
+++ venta_movil/static/src/js/custom.js
@@ -1,25 +1,25 @@
 odoo.define('pos.custom_button', function (require) {
-'use strict';
+    'use strict';
     var core = require('web.core');
     var screens = require('point_of_sale.screens');
     var gui = require('point_of_sale.gui');
 
     var CustomButton = screens.ActionButtonWidget.extend({
-        template : 'CustomButton',
+        template: 'CustomButton',
 
-        button_click : function(){
+        button_click: function () {
             var self = this;
-            self.gui.show_popup('textinput',{
-                'title':'Prestamo',
-                'confirm':function(){
-                  console.log('Hello')
+            self.gui.show_popup('numberinput', {
+                'title': 'Prestamo',
+                'confirm': function () {
+                    console.log(this.$('#numberinput').val());
                 },
-                'body':'¿Esta seguro de realizar un prestamo?'
+                'body': '¿Esta seguro de realizar un prestamo?'
             })
         },
     });
     screens.define_action_button({
-        'name':'custom_button',
-        'widget':CustomButton
+        'name': 'custom_button',
+        'widget': CustomButton
     })
 });
