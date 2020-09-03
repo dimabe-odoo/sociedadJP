2a24c83a80bc43f26faf5b0660c7fdaa30448031
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-03 09:17:52 -0400

    test

--- venta_movil/static/src/js/custom.js
+++ venta_movil/static/src/js/custom.js
@@ -3,7 +3,7 @@ odoo.define('pos.custom_button', function (require) {
     var core = require('web.core');
     var screens = require('point_of_sale.screens');
     var gui = require('point_of_sale.gui');
-
+    var module = require('point_of_sale.models');
     var CustomButton = screens.ActionButtonWidget.extend({
         template: 'CustomButton',
 
@@ -14,6 +14,8 @@ odoo.define('pos.custom_button', function (require) {
                 'confirm': function () {
                     var value = this.$('.active');
                     console.log(value[0].innerHTML);
+                    console.log(this.module)
+                    console.log(module)
                 },
                 'body': '¿Esta seguro de realizar un prestamo?'
             })
