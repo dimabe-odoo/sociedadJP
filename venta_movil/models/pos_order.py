1456dde4aeda7ca7e88d5074da7e7b59a19fca6b
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-03 08:51:06 -0400

    test

--- venta_movil/models/pos_order.py
+++ venta_movil/models/pos_order.py
@@ -57,3 +57,7 @@ class PosOrder(models.Model):
             else:
                 continue
         return res
+
+    @api.model
+    def create_from_ui(self,orders,draft=False):
+        raise models.ValidationError(orders)
