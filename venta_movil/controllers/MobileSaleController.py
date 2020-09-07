1f16e6023153ab09ec92e64de074bf2ae32e28d2
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-07 10:52:01 -0300

    add mobile sale models and mobile sale controller for integration with the app

---
+++ venta_movil/controllers/MobileSaleController.py
@@ -0,0 +1,12 @@
+from odoo import http
+from odoo.http import request
+import datetime
+
+class MobileSaleController(http.Controller):
+
+    @http.route('/api/create_sale',type='json',method=['POST'],auth='public',cors='*')
+    def create_sale(self,customer_id,saleman_id,product_id):
+        customer_id = request.env['res.partner'].sudo().search([('id','=',customer_id)])
+        saleman_id = request.env['res.partner'].sudo().search([('id','=',saleman_id)])
+
+        return {'customer':customer_id.name,'saleman_id':saleman_id.name}
--- venta_movil/models/mobile_sale_order.py
+++ venta_movil/models/mobile_sale_order.py
@@ -1,9 +1,19 @@
 from odoo import fields, models, api
 
 
-class JpSaleOrder(models.Model):
-    _name = 'jp.sale.order'
+class ModileSaleOrder(models.Model):
+    _name = 'mobile.sale.order'
 
     name = fields.Char('Nombre')
 
-    
+    state = fields.Selection([('progress','En Progeso'),('done','Hecha')])
+
+    customer_id = fields.Many2one('res.partner','Cliente')
+
+    saleman_id = fields.Many2one('res.partner','Vendedor')
+
+    date_done = fields.Datetime('Fecha de entrega')
+
+    product_id = fields.Many2one('product_id','Producto')
+
+    sale_id = fields.Many2one('sale.order','Venta Interna')
