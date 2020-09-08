c84bc30facfda47e3dd7bd858abb05b7518592fe
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-08 13:52:13 -0300

    test

--- venta_movil/models/stock_warehouse.py
+++ venta_movil/models/stock_warehouse.py
@@ -5,6 +5,12 @@ class StockWarehouse (models.Model):
     _inherit = 'stock.warehouse'
 
     loan_location_id = fields.Many2one('stock.location','Ubicacion de Prestamo')
+
+    is_truck = fields.Boolean('¿Es camion?')
+
+    user_id = field.Many2one('res.user')
+
+
     
 
 
