a9cdede6336f6dde0a10ccf845f61576631f0508
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-09 11:21:56 -0300

    test

--- venta_movil/models/mobile_line.py
+++ venta_movil/models/mobile_line.py
@@ -3,14 +3,14 @@ from odoo import fields,models,api
 class MobileSaleLine(models.Model):
     _name = 'mobile.sale.line'
 
-    reference = fields.Char('Referencia')
+    reference = fields.Char('Referencia',ref='mobile_id.name')
 
-    product_id = fields.Many2one('Producto')
+    product_id = fields.Many2one('product.product','Producto')
 
     price = fields.Float('Precio')
 
-    state = fields.Selection([('progress','En Progeso'),('done','Hecha')])
+    state = fields.Selection([('progress','En Progeso'),('done','Hecha')],default='progress')
 
     qty = fields.Integer('Cantidad')
 
-    mobile_id = fields.Many2one('mobile.sale.order')
+    mobile_id = fields.Many2one('mobile.sale.order',auto_join=True)
