248128efb8cc294e53ac7e56ff0235e749240c7e
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-09 10:58:07 -0300

    test

---
+++ venta_movil/models/mobile_line.py
@@ -0,0 +1,16 @@
+from odoo import fields,models,api
+
+class MobileSaleLine(models.Model):
+    _name = 'mobile.sale.line'
+
+    reference = fields.Char('Referencia')
+
+    product_ids = fields.Many2one('Producto')
+
+    price = fields.Float('Precio')
+
+    state = fields.Selection([('progress','En Progeso'),('done','Hecha')])
+
+    qty = fields.Integer('Cantidad')
+
+    mobile_id = fields.Many2one('mobile.sale.order')
