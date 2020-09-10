d3cb7d6b2101586a9408d240a2ea4f9da6db3327
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-10 08:58:14 -0300

    test

--- venta_movil/static/src/js/pos_discount.js
+++ venta_movil/static/src/js/pos_discount.js
@@ -1,9 +1,17 @@
 odoo.define('pos_discount.andes',function (require) {
     var screens = require('point_of_sale.screens');
+    var models = require('point_of_sale.models');
+    var _super_Order = models.Order.prototype;
     var discount_button = screens.ActionButtonWidget.extend({
         template : 'BtnDiscount',
         button_click : function (){
             var order = this.pos.get_order();
+            models.Order = models.Order.extend({
+                export_as_JSON : function (){
+                    var json = _super_Order.export_as_JSON.apply(this,arguments);
+                    console.log(json)
+                }
+            })
             order.selected_orderline['loan'] = 5
             if (order.selected_orderline){
                 console.log(order.orderlines.filter(a => a.cid == order.selected_orderline.cid));
