b39a8de39eeb44fa097bd472331287f446c418c2
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-10 17:29:15 -0300

    test

--- venta_movil/static/src/js/pos_discount.js
+++ venta_movil/static/src/js/pos_discount.js
@@ -12,7 +12,8 @@ odoo.define('pos_discount.andes', function (require) {
             var json = _super_order.export_as_JSON.apply(this, arguments);
             if (json.lines) {json.lines.forEach(function (e) {
                     e.forEach(function (a) {
-                        a.loan = self.pos.loan
+                        if(a.product_id === self.selected_product)
+                            a.loan = self.pos.loan
                     })
                 })
             }
