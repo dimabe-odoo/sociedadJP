6c6396fce41ee39054f035ca05cbb765b58e87b3
dimabe-odoo <desarrolloti@dimabe.cl>
2020-09-11 15:26:08 -0300

    test

--- venta_movil/models/pos_order.py
+++ venta_movil/models/pos_order.py
@@ -5,7 +5,9 @@ import datetime
 class PosOrder(models.Model):
     _inherit = 'pos.order'
 
-    supply_reception_id = fields.Many2one('stock.picking', 'Recepcion de Insumos')
+    supply_reception_id = fields.Many2one('stock.picking', 'Recepcion de insumos')
+
+    loan_reception_id = fields.Many2one('stock.picking', 'Prestamo de insumos')
 
     is_loan = fields.Boolean()
 
@@ -17,18 +19,17 @@ class PosOrder(models.Model):
     def create_picking(self):
         res = super(PosOrder, self).create_picking()
         if self.lines.filtered(lambda l: l.product_id.supply_id):
-            models._logger.error(
-                'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa{}'.format(
-                    len(self.lines)))
             if self.is_loan:
-                reception_id = self.env['stock.picking'].create({
-                    'name': 'POS/IN/{}'.format(self.name),
-                    'picking_type_id': self.env['stock.picking.type'].search(
-                        [('warehouse_id.id', '=', self.picking_type_id.warehouse_id.id),
-                         ('sequence_code', '=', 'IN')]).id,
+                loan_id = self.env['stock.picking'].create({
+                    'name': 'POS/LOAN/{}'.format(self.name),
+                    'picking_type_id': self.env['stock.picking.type'].search([
+                        ('warehouse_id,id', '=', self.picking_type_id.warehouse_id.id),
+                        ('sequence_code', '=', 'IN')
+                    ]).id,
                     'location_id': self.env['stock.location'].search([('name', '=', 'Customers')]).id,
-                    'location_dest_id': self.env['stock.warehouse'].search(
-                        [('id', '=', self.picking_type_id.warehouse_id.id)]).loan_location.id,
+                    'location_dest_id': self.env['stock.warehouse'].search([
+                        ('id', '=', self.picking_type_id.warehouse_id.id).loan_location_id.id
+                    ]),
                     'move_type': 'direct',
                     'picking_type_code': 'incoming',
                     'state': 'assigned',
@@ -37,32 +38,44 @@ class PosOrder(models.Model):
                     'origin': 'Entrada de {}'.format(self.name),
                     'partner_id': self.partner_id.id
                 })
-            else:
-                reception_id = self.env['stock.picking'].create({
-                    'name': 'POS/IN/{}'.format(self.name),
-                    'picking_type_id': self.env['stock.picking.type'].search(
-                        [('warehouse_id.id', '=', self.picking_type_id.warehouse_id.id),
-                         ('sequence_code', '=', 'IN')]).id,
-                    'location_id': self.env['stock.location'].search([('name', '=', 'Customers')]).id,
-                    'location_dest_id': self.location_id.id,
-                    'move_type': 'direct',
-                    'picking_type_code': 'incoming',
-                    'state': 'assigned',
-                    'date_done': datetime.datetime.now(),
-                    'company_id': self.env.user.company_id.id,
-                    'origin': 'Entrada de {}'.format(self.name),
-                    'partner_id': self.partner_id.id
-                })
-            self.write({
-                'supply_reception_id': reception_id.id,
+            reception_id = self.env['stock.picking'].create({
+                'name': 'POS/IN/{}'.format(self.name),
+                'picking_type_id': self.env['stock.picking.type'].search([
+                    ('warehouse_id,id', '=', self.picking_type_id.warehouse_id.id),
+                    ('sequence_code', '=', 'IN')
+                ]).id,
+                'location_id': self.env['stock.location'].search([('name', '=', 'Customers')]).id,
+                'location_dest_id': self.env['stock.warehouse'].search([
+                    ('id', '=', self.picking_type_id.warehouse_id.id).lot_stock_id.id
+                ]),
+                'move_type': 'direct',
+                'picking_type_code': 'incoming',
+                'state': 'assigned',
+                'date_done': datetime.datetime.now(),
+                'company_id': self.env.user.company_id.id,
+                'origin': 'Entrada de {}'.format(self.name),
+                'partner_id': self.partner_id.id
             })
-        else:
-            return res
-        for line in self.lines:
-            if line.product_id.supply_id:
-                if line.loan > 0:
+            for line in self.lines:
+                if self.is_loan and line.product_id.supply_id.id:
                     qty = (line.qty - line.loan)
-                    models._logger.error(qty)
+                    loan_move = self.env['stock.move'].create({
+                        'name': loan_id.name,
+                        'picking_id': loan_id.id,
+                        'location_id': loan_id.location_id.id,
+                        'location_dest_id': loan_id.location_dest_id.id,
+                        'product_id': line.product_id.supply_id.id,
+                        'date': datetime.datetime.now(),
+                        'company_id': loan_id.company_id.id,
+                        'procure_method': 'make_to_stock',
+                        'quantity_done': qty,
+                        'product_uom': line.product_id.supply_id.uom_id.id
+                    })
+                    self.write({
+                        'loan_reception_id': loan_id.id
+                    })
+                    self.loan_reception_id.button_validate()
+                if line.product_id.supply_id.id:
                     stock_move = self.env['stock.move'].create({
                         'name': reception_id.name,
                         'picking_id': reception_id.id,
@@ -75,30 +88,9 @@ class PosOrder(models.Model):
                         'quantity_done': qty,
                         'product_uom': line.product_id.supply_id.uom_id.id,
                     })
-                else:
-                    stock_move = self.env['stock.move'].create({
-                        'name': reception_id.name,
-                        'picking_id': reception_id.id,
-                        'location_id': reception_id.location_id.id,
-                        'location_dest_id': reception_id.location_dest_id.id,
-                        'product_id': line.product_id.supply_id.id,
-                        'date': datetime.datetime.now(),
-                        'company_id': reception_id.company_id.id,
-                        'procure_method': 'make_to_stock',
-                        'quantity_done': line.qty,
-                        'product_uom': line.product_id.supply_id.uom_id.id,
+                    self.write({
+                        'supply_reception_id':reception_id.id
                     })
-                # self.env['stock.move.line'].create({
-                #     'move_id': stock_move.id,
-                #     'company_id': stock_move.company_id.id,
-                #     'date': stock_move.date,
-                #     'location_id': stock_move.location_id.id,
-                #     'location_dest_id': stock_move.location_dest_id.id,
-                #     'product_id': stock_move.product_id.id,
-                #     'product_uom_id': stock_move.product_uom.id,
-                #     'qty_done': stock_move.quantity_done
-                # })
-                reception_id.button_validate()
-            else:
-                continue
-        return res
+                    self.supply_reception_id.button_validate()
+                else:
+                    continue
