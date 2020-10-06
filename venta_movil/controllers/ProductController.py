c78407ea91fa555d9c10ab6ca1a073a962865c87
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-06 11:20:15 -0300

    test

--- venta_movil/controllers/ProductController.py
+++ venta_movil/controllers/ProductController.py
@@ -1,18 +1,22 @@
 from odoo import http
 from odoo.http import request
 
+
 class ProductController(http.Controller):
 
-    @http.route('/api/products',type='json',method='GET',auth='token',cors='*')
+    @http.route('/api/products', type='json', method='GET', auth='token', cors='*')
     def get_products(self):
         result = request.env['product.product'].search([])
         data = []
         for res in result:
+            price = request.env['product.pricelist'].search(
+                [('name', '=', 'Tarifa Publica Reparto')]).item_ids.filtered(
+                lambda a: a.product_tmpl_id.id == res.product_tmpl_id.id).fixed_price
             data.append({
                 'id': res.id,
                 'name': res.name,
-                #'price': res.fixed_price,
-                'photo':res.image_1024
+                'price': price,
+                'photo': res.image_1024
             })
 
         return data
