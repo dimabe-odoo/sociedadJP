ac4be5e4605bd35d527fd6c34601ada39f2dea35
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-06 15:38:30 -0300

    test

--- venta_movil/controllers/ProductController.py
+++ venta_movil/controllers/ProductController.py
@@ -6,7 +6,7 @@ class ProductController(http.Controller):
 
     @http.route('/api/get_products', type='json', method='GET', auth='token', cors='*')
     def get_products(self):
-        result = request.env['res.users'].sudo().search([('id', '=', request.uid)])[0].partner_id.property_product_pricelist
+        result = request.env['res.users'].sudo().search([('id', '=', request.uid)])[0].partner_id.property_product_pricelist.item_ids
         data = []
         for res in result:
             data.append({
