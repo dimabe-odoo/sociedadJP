9bd49eb4c7d1ffe73a3085057a25e23755a7d2b3
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-07 12:34:57 -0300

    test

--- venta_movil/controllers/AddressController.py
+++ venta_movil/controllers/AddressController.py
@@ -3,6 +3,7 @@ from odoo.http import request
 from datetime import date
 import werkzeug
 
+
 class AddressController(http.Controller):
 
     @http.route('/api/addresses', type='json', methods=['POST'], auth='token', cors='*')
@@ -11,12 +12,26 @@ class AddressController(http.Controller):
         if not partner_id:
             raise werkzeug.exceptions.BadRequest('Imposible encontrar el contacto asociado al usuario')
 
-        partner_id.write({'state_id': 1176,'jp_commune_id': int(commune_id), 'city': city, 'street': address, 'partner_latitude': float(latitude), 'partner_longitude': float(longitude), 'date_localization':date.today()})
+        partner_id.write({'state_id': 1176, 'jp_commune_id': int(commune_id), 'city': city, 'street': address,
+                          'partner_latitude': float(latitude), 'partner_longitude': float(longitude),
+                          'date_localization': date.today()})
 
         return {'message': 'Dirección creada correctamente'}
 
-    @http.route('/api/get_address',type='json',methods=['POST'],auth='token',cors='*')
-    def add_address(self,partner_id):
-        partner_id = request.env['res.partner'].sudo().search([('id','=',partner_id)])
-        return partner_id.child_ids
-
+    @http.route('/api/get_address', type='json', methods=['POST'], auth='token', cors='*')
+    def add_address(self, partner_id):
+        partner_id = request.env['res.partner'].sudo().search([('id', '=', partner_id)])
+        res = []
+        for child in partner_id.child_ids:
+            res.append({
+                {
+                    "id": child.id,
+                    "partner_Id": partner_id.id,
+                    "city": child.city,
+                    "commune": child.jp_commune_id.name,
+                    "references": child.comment,
+                    "name": child.name,
+                    "street": child.street
+                }
+            })
+        return res
