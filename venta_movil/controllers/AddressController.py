ddc2d971e2bae27954de55a7e44060d205418d68
dimabe-odoo <desarrolloti@dimabe.cl>
2020-10-15 16:23:01 -0300

    modify add_address

--- venta_movil/controllers/AddressController.py
+++ venta_movil/controllers/AddressController.py
@@ -55,4 +55,8 @@ class AddressController(http.Controller):
     @http.route('/api/delete_address',type='json',method=['POST'],auth='token',cors='*')
     def delete(self,partner_id):
         partner_id = request.env['res.partner'].search([('id','=',partner_id)])
-        partner_id.sudo().unlink()
+        partner_id.sudo().write({
+            'parent_id': None
+        })
+
+        return {'message':'Direccion eliminada correctamente'}
