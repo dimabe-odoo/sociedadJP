c0bfe8a2c82a31ffc2efb2533481245fa4440473
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-19 17:05:25 -0300

    verify pos gui product list widget

--- venta_movil/static/src/js/pos_discount.js
+++ venta_movil/static/src/js/pos_discount.js
@@ -66,7 +66,9 @@ odoo.define('pos_discount.andes', function (require) {
             console.log('POS.GUI')
             console.log(this.pos.gui)
             console.log('POS.GUI.CURRENT_SCREEN')
-            console.log(this.pos.gui.pos.gui.current_screen.product_list)
+            if(this.pos.gui.pos.gui.current_screen){
+                console.log(this.pos.gui.pos.gui.current_screen.product_list)
+            }
             return json;
         }
     })
