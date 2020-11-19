e74e239389d4405f666f4b395a7a0179af787f65
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-19 16:45:05 -0300

    verify pos gui product list widget

--- venta_movil/static/src/js/pos_discount.js
+++ venta_movil/static/src/js/pos_discount.js
@@ -2,81 +2,19 @@ odoo.define('pos_discount.andes', function (require) {
     var screens = require('point_of_sale.screens');
     var models = require('point_of_sale.models');
     screens.ProductListWidget.include({
-        init: function(parent, options) {
-            var self = this;
-            this._super(parent,options);
-            this.model = options.model;
-            this.productwidgets = [];
-            this.weight = options.weight || 0;
-            this.show_scale = options.show_scale || false;
-            this.next_screen = options.next_screen || false;
-            this.search_word = false;
-    
-            this.click_product_handler = function(){
-                var product = self.pos.db.get_product_by_id(this.dataset.productId);
-                options.click_product_action(product);
-            };
-    
-            this.keypress_product_handler = function(ev){
-                // React only to SPACE to avoid interfering with warcode scanner which sends ENTER
-                if (ev.which != 32) {
-                    return;
-                }
-                ev.preventDefault();
-                var product = self.pos.db.get_product_by_id(this.dataset.productId);
-                options.click_product_action(product);
-            };
-    
-            this.product_list = options.product_list || [];
-            this.product_cache = new DomCache();
-            this.pos.get('orders').bind('add remove change', function () {
-                self.renderElement();
-            }, this);
-    
-            this.pos.bind('change:selectedOrder', function () {
-                this.renderElement();
-            }, this);
-        },
-        set_product_list: function(product_list, search_word){
-            console.log(product_list)
-            this.product_list = product_list;
-            this.search_word = !!search_word ? search_word : false;
-            this.renderElement();
-        },
-        get_product_image_url: function(product){
-            return window.location.origin + '/web/image?model=product.product&field=image_128&id='+product.id;
-        },
-        replace: function($target){
-            this.renderElement();
-            var target = $target[0];
-            target.parentNode.replaceChild(this.el,target);
-        },
-        calculate_cache_key: function(product, pricelist){
-            return product.id + ',' + pricelist.id;
-        },
-        _get_active_pricelist: function(){
-            var current_order = this.pos.get_order();
-            var current_pricelist = this.pos.default_pricelist;
-    
-            if (current_order) {
-                current_pricelist = current_order.pricelist;
-            }
-    
-            return current_pricelist;
-        },
         render_product: function(product){
             var current_pricelist = this._get_active_pricelist();
             var cache_key = this.calculate_cache_key(product, current_pricelist);
             var cached = this.product_cache.get_node(cache_key);
             if(!cached){
                 var image_url = this.get_product_image_url(product);
-
                 var product_html = QWeb.render('Product',{ 
                         widget:  this, 
                         product: product,
                         pricelist: current_pricelist,
                         image_url: this.get_product_image_url(product),
                     });
+                console.log(product)
                 var product_node = document.createElement('div');
                 product_node.innerHTML = product_html;
                 product_node = product_node.childNodes[1];
@@ -84,27 +22,7 @@ odoo.define('pos_discount.andes', function (require) {
                 return product_node;
             }
             return cached;
-        },
-    
-        renderElement: function() {
-            var el_str  = QWeb.render(this.template, {widget: this});
-            var el_node = document.createElement('div');
-                el_node.innerHTML = el_str;
-                el_node = el_node.childNodes[1];
-    
-            if(this.el && this.el.parentNode){
-                this.el.parentNode.replaceChild(el_node,this.el);
-            }
-            this.el = el_node;
-    
-            var list_container = el_node.querySelector('.product-list');
-            for(var i = 0, len = this.product_list.length; i < len; i++){
-                var product_node = this.render_product(this.product_list[i]);
-                product_node.addEventListener('click',this.click_product_handler);
-                product_node.addEventListener('keypress',this.keypress_product_handler);
-                list_container.appendChild(product_node);
-            }
-        },
+        }
     });
     var _super_order = models.Order.prototype;
     var loan = 0
