7001f377c39936537ba1c016776fb5df0c7cac34
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-20 09:46:39 -0300

    test

--- venta_movil/static/src/js/pos_discount.js
+++ venta_movil/static/src/js/pos_discount.js
@@ -70,7 +70,7 @@ odoo.define('pos_discount.andes', function (require) {
                             if(order.pricelist){
                                 console.log("ELEMENT")
                                 console.log(element)
-                                console.log(order.pricelist.item[0])
+                                console.log(order.pricelist.items[0])
                                 // var price = order.pricelist.items.filter(function (product) {
                                 //     return product[1].id == element.id;
                                 // }).fixed_price
