2772d19715ce87fd1226ea48a390fa40e52bc194
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-23 15:28:10 -0300

    test

--- venta_movil/static/src/js/pos_discount.js
+++ venta_movil/static/src/js/pos_discount.js
@@ -112,7 +112,10 @@ odoo.define('pos_discount.andes', function (require) {
                                         if (item.product_tmpl_id[0] == element.id) {
                                             var price = ((19 / 100) * parseFloat(item.price.split(' ')[0].split(".")[0]))
                                             console.log(order.selected_orderline)
-                                            order.selected_orderline.price = order.selected_orderline.price - price
+                                            
+                                            order.selected_orderline.price = order.selected_orderline.price - price;
+                                            order.selected_orderline.product.taxes_id = []
+                                            this.el.querySelector('.summary .total .subentry .value').textContent = 3040;
                                         }
                                     })
                                     // var price = order.pricelist.items.filter(function (product) {
