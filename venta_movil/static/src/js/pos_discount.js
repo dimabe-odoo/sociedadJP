e5d47d80b1277682d90d6bed80a10c32f4969ee9
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-23 15:15:49 -0300

    test

--- venta_movil/static/src/js/pos_discount.js
+++ venta_movil/static/src/js/pos_discount.js
@@ -110,7 +110,7 @@ odoo.define('pos_discount.andes', function (require) {
                                     
                                     order.pricelist.items.forEach(item => {
                                         if (item.product_tmpl_id[0] == element.id) {
-                                            var price = ((19 / 100) * item.price)
+                                            var price = ((19 / 100) * parseFloat(item.price.split(' ')[0].split(".")[0]))
                                             console.log(price)
                                         }
                                     })
