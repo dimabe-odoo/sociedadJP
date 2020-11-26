94ccf8bd0bab710ce468bfab882cac5c0ac55827
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-26 15:22:56 -0300

    test

--- venta_movil/models/mobile_sale_order.py
+++ venta_movil/models/mobile_sale_order.py
@@ -137,6 +137,7 @@ class MobileSaleOrder(models.Model):
                 })
         sale_odoo.picking_ids[0].button_validate()
         sale_odoo._create_invoices()
+        sale_odoo.invoice_ids[0].action_post()
         self.write({
             'sale_id': sale_odoo.id,
             'state':'done'
