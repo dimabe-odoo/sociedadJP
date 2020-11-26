28e2d5ce9606c24c99f218392e5976ef7cc4d326
dimabe-odoo <desarrolloti@dimabe.cl>
2020-11-26 15:52:10 -0300

    test

--- venta_movil/models/truck_session.py
+++ venta_movil/models/truck_session.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class TruckSession(models.Model):
     _name = 'truck.session'
 
-    login_datetime = fields.DateTime('Fecha de ultimo ingreso')
+    login_datetime = fields.Datetime('Fecha de ultimo ingreso')
 
     user_id = fields.Many2one('res.users', 'Usuario')
 
