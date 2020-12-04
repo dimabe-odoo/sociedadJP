2a2f3a1dec3169defddbd65b87789723b5a10e0f
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-04 09:44:48 -0300

    test

--- venta_movil/models/mobile_sale_order.py
+++ venta_movil/models/mobile_sale_order.py
@@ -108,13 +108,13 @@ class MobileSaleOrder(models.Model):
             'location_id').filtered(lambda a: a.is_truck)
         self.truck_ids = stock_quant
 
-    @api.onchange('mobile_lines')
+    @api.onchange('total_sale')
     def compute_total_untaxed(self):
         tax = self.mobile_lines.mapped('product_id').mapped('taxes_id')[0].amount / 100
         untaxed = self.total_sale / ( 1 + tax)
         self.total_untaxed = untaxed
 
-    @api.onchange('mobile_lines')
+    @api.onchange('total_sale')
     def compute_total_taxes(self):
         tax = self.mobile_lines.mapped('product_id').mapped('taxes_id')[0].amount / 100
         taxes = self.total_untaxed * tax
