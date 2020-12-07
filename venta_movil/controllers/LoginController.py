29cb6bb3489f6b91c2d55b7c8f6bc2faab13b85f
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-07 17:42:50 -0300

    test

--- venta_movil/controllers/LoginController.py
+++ venta_movil/controllers/LoginController.py
@@ -47,10 +47,10 @@ class LoginController(http.Controller):
                     'mobile': user[0].mobile, 'token': token, 'address': user[0].street}
 
     @http.route('/api/assign_truck',type="json",method=['GET'],auth='token',cors='*')
-    def assign_truck(self,truck,user,employee):
+    def assign_truck(self,truck,employee):
         truck = request.env['stock.location'].search([('name','=',truck)])
         session = request.env['truck.session'].create({
-            'user_id':user,
+            'user_id':uid,
             'truck_i':truck.id,
             'employee_id':employee,
             'is_login':True,
