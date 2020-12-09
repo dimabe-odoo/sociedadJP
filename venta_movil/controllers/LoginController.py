fc8daf400fe7cad209fd1acd15b105b91a1ba8f5
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-09 09:45:50 -0300

    test

--- venta_movil/controllers/LoginController.py
+++ venta_movil/controllers/LoginController.py
@@ -3,59 +3,58 @@ from odoo.http import request
 from ..jwt_token import generate_token
 import datetime
 
+
 class LoginController(http.Controller):
     @http.route('/api/login', type='json', auth='public', cors='*')
-    def do_login(self, user, password,is_driver= False):
-        if not is_driver:
-            uid = request.session.authenticate(
-                request.env.cr.dbname,
-                user,
-                password
-            )
-            if not uid:
-                return self.errcode(code=400, message='incorrect login')
+    def do_login(self, user, password):
+        uid = request.session.authenticate(
+            request.env.cr.dbname,
+            user,
+            password
+        )
+        if not uid:
+            return self.errcode(code=400, message='incorrect login')
 
-            token = generate_token(uid)
+        token = generate_token(uid)
 
-            user = request.env['res.users'].browse(uid)[0]
+        user = request.env['res.users'].browse(uid)[0]
 
-            if len(request.env['sale.order'].search([('partner_id', '=', user[0].partner_id.id)])) > 1:
-                last_order = request.env['sale.order'].search([('partner_id', '=', user[0].partner_id.id)])[-1]
-            else:
-                last_order = 'No tiene pedido asociados'
+        employee_id = request.env['hr.employee'].sudo().search([('user_id', '=', user.id)])
 
-            return {'user': user[0].name, 'last_order': last_order,
-                    'partner_id': user[0].partner_id.id, 'email': user[0].email, 'rut': user[0].vat,
-                    'mobile': user[0].mobile, 'token': token, 'address': user[0].street}
-        else:
-            uid = request.session.authenticate(
-                request.env.cr.dbname,
-                user,
-                password
-            )
-            if not uid:
-                return self.errcode(code=400, message='incorrect login')
+        return {'user_id': user[0].id, 'user': user[0].name, 'employee_id': employee_id.id,
+                'partner_id': user[0].partner_id.id, 'email': user[0].email, 'rut': user[0].vat,
+                'mobile': user[0].mobile, 'token': token, 'address': user[0].street}
 
-            token = generate_token(uid)
+    @http.route('/api/login_truck', type="json", method=['GET'], auth='public', cors='*')
+    def do_login_truck(self, user, password):
+        uid = request.session.authenticate(
+            request.env.cr.dbname,
+            user,
+            password
+        )
+        if not uid:
+            return self.errcode(code=400, message='incorrect login')
 
-            user = request.env['res.users'].browse(uid)[0]
+        token = generate_token(uid)
 
-            employee_id = request.env['hr.employee'].sudo().search([('user_id','=',user.id)])
+        user = request.env['res.users'].browse(uid)[0]
 
-            return {'user_id':user[0].id,'user': user[0].name,'employee_id':employee_id.id,
-                    'partner_id': user[0].partner_id.id, 'email': user[0].email, 'rut': user[0].vat,
-                    'mobile': user[0].mobile, 'token': token, 'address': user[0].street}
+        employee_id = request.env['hr.employee'].sudo().search([('user_id', '=', user.id)])
 
-    @http.route('/api/assign_truck',type="json",method=['GET'],auth='token',cors='*')
-    def assign_truck(self,truck,employee,user,vat):
-        truck = request.env['stock.location'].sudo().search([('name','=',truck)])
+        return {'user_id': user[0].id, 'user': user[0].name, 'employee_id': employee_id.id,
+                'partner_id': user[0].partner_id.id, 'email': user[0].email, 'rut': user[0].vat,
+                'mobile': user[0].mobile, 'token': token, 'address': user[0].street}
+
+    @http.route('/api/assign_truck', type="json", method=['GET'], auth='token', cors='*')
+    def assign_truck(self, truck, employee, user, vat):
+        truck = request.env['stock.location'].sudo().search([('name', '=', truck)])
         if not employee:
-            employee_id = request.env['hr.employee'].search([('user_id','=',user)])
+            employee_id = request.env['hr.employee'].search([('user_id', '=', user)])
             session = request.env['truck.session'].sudo().create({
-                'user_id':user,
-                'truck_id':truck.id,
-                'employee_id':employee_id.id,
-                'is_login':True,
+                'user_id': user,
+                'truck_id': truck.id,
+                'employee_id': employee_id.id,
+                'is_login': True,
             })
         else:
             session = request.env['truck.session'].sudo().create({
@@ -64,7 +63,7 @@ class LoginController(http.Controller):
                 'employee_id': employee,
                 'is_login': True,
             })
-        return {'session_id':session.id}
+        return {'session_id': session.id}
 
     @http.route('/api/refresh-token', type='json', auth='public', cors='*')
     def do_refresh_token(self, email):
