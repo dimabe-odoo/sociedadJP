2b6d00b65c9213278fd456c750655e488c2c0040
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-10 09:20:43 -0300

    test

--- venta_movil/controllers/LoginController.py
+++ venta_movil/controllers/LoginController.py
@@ -46,7 +46,7 @@ class LoginController(http.Controller):
         if session:
             return {'user_id': str(user[0].id), 'user': user[0].name, 'employee_id': str(employee_id.id),
                     'partner_id': user[0].partner_id.id, 'email': user[0].email, 'rut': user[0].vat,
-                    'mobile': user[0].mobile, 'token': token, 'address': user[0].street,'session':session.id}
+                    'mobile': user[0].mobile, 'token': token, 'address': user[0].street,'session':str(session.id)}
         else:
             return {'user_id': str(user[0].id), 'user': user[0].name, 'employee_id': str(employee_id.id),
                     'partner_id': user[0].partner_id.id, 'email': user[0].email, 'rut': user[0].vat,
