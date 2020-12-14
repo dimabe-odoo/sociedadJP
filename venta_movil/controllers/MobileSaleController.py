320b2e0ba46449dd6253af87190f9c11e9682df9
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-14 17:39:29 -0300

    test

--- venta_movil/controllers/MobileSaleController.py
+++ venta_movil/controllers/MobileSaleController.py
@@ -86,7 +86,7 @@ class MobileSaleController(http.Controller):
                     array_srt_des.append('{} {}s'.format(product.qty, product.product_id.name))
                     array_des.append({
                         'Id':product.id,
-                        'ImageUrl': '/web/image?model=product.product&field:imagen_128&id={}'.format(
+                        'ImageUrl': '/web/image?model=product.product&field:image_128&id={}'.format(
                             product.product_id.id),
                         'Product_Id':product.product_id.id,
                         'ProductName': product.product_id.name,
@@ -97,7 +97,7 @@ class MobileSaleController(http.Controller):
                     array_srt_des.append('{} {}'.format(product.qty, product.product_id.name))
                     array_des.append({
                         'Id':product.id,
-                        'ImageUrl':'/web/image?model=product.product&field:imagen_128&id={}'.format(product.product_id.id),
+                        'ImageUrl':'/web/image?model=product.product&field:image_128&id={}'.format(product.product_id.id),
                         'Product_Id':product.product_id.id,
                         'ProductName': product.product_id.name,
                         'Qty': product.qty,
@@ -166,7 +166,7 @@ class MobileSaleController(http.Controller):
                     array_srt_des.append('{} {}'.format(product.qty, product.product_id.name))
                     array_des.append({
                         'Id': product.id,
-                        'ImageUrl': '/web/image?model=product.product&field:imagen_128&id={}'.format(
+                        'ImageUrl': '/web/image?model=product.product&field:image_128&id={}'.format(
                             product.product_id.id),
                         'Product_Id': product.product_id.id,
                         'ProductName': product.product_id.name,
