2e7dcff2f32e58d61b0b7ca73a2d187261364898
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-17 12:43:26 -0300

    test

--- venta_movil/controllers/ResPartnerController.py
+++ venta_movil/controllers/ResPartnerController.py
@@ -36,7 +36,7 @@ class ResPartnerController(http.Controller):
             for pr in res.property_product_pricelist.item_ids:
                 price.append({
                     'Product_Id': pr.product_tmlp_id.id,
-                    'Product_Name': pr.product_tmlp_id.display_name,
+                    'Product_Name': pr.product_tmpl_id.display_name,
                     'Price': pr.fixed_price
                 })
 
