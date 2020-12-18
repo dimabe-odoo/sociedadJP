711fe02c6446f96fb7b7a1140e8ec33a7d522362
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-18 08:40:42 -0300

    test

--- venta_movil/controllers/MobileSaleController.py
+++ venta_movil/controllers/MobileSaleController.py
@@ -100,6 +100,7 @@ class MobileSaleController(http.Controller):
                                    (res.customer_id.partner_latitude, res.customer_id.partner_longitude),
                                    mode="driving",
                                    departure_time=now)
+            _logger.error('Dir : {}'.format(dir))
             for product in res.mobile_lines:
                 if product.qty > 1:
                     array_srt_des.append('{} {}s'.format(product.qty, product.product_id.name))
