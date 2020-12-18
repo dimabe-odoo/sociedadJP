59adb71d63199119619e407a9716b6b70ae589a4
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-18 09:15:23 -0300

    test

--- venta_movil/controllers/ResPartnerController.py
+++ venta_movil/controllers/ResPartnerController.py
@@ -8,8 +8,10 @@ import googlemaps
 class ResPartnerController(http.Controller):
 
     @http.route('/api/clients', type='json', method=['GET'], auth='token', cors='*')
-    def get_clients(self):
+    def get_clients(self,truck):
         respond = request.env['res.partner'].search([])
+        location = request.env['stock.location'].sudo().search([('name','=',truck)])
+        stock = request.env['stock.quant'].sudo().search([('location_id','=',location.id)])
         result = []
         now = datetime.datetime.now()
         _logger = logging.getLogger(__name__)
@@ -18,10 +20,12 @@ class ResPartnerController(http.Controller):
             for c in res.child_ids:
                 price_another = []
                 for pr in res.property_product_pricelist.item_ids:
+                    stock_product = stock.filtered(lambda a: a.product_id == pr.product_tmpl_id.id)
                     price_another.append({
                         'Product_Id': pr.product_tmpl_id.id,
                         'Product_Name': pr.product_tmpl_id.name,
                         'isCat': True if 'Catalítico' in pr.product_tmpl_id.display_name else False,
+                        'Stock':stock_product.quantity,
                         'Price': pr.fixed_price
                     })
                 another.append({
@@ -35,10 +39,12 @@ class ResPartnerController(http.Controller):
                 })
             price = []
             for pr in res.property_product_pricelist.item_ids:
+                stock_product = stock.filtered(lambda a: a.product_id == pr.product_tmpl_id.id)
                 price.append({
                     'Product_Id': pr.product_tmpl_id.id,
                     'Product_Name': pr.product_tmpl_id.name,
                     'isCat': True if 'Catalítico' in pr.product_tmpl_id.display_name else False,
+                    'Stock': stock_product.quantity,
                     'Price': pr.fixed_price
                 })
 
