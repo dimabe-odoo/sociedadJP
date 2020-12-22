951d236ad635b7be4a4c42024c546879a3ea1e19
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-22 11:42:57 -0300

    test

--- venta_movil/controllers/MobileSaleController.py
+++ venta_movil/controllers/MobileSaleController.py
@@ -132,10 +132,7 @@ class MobileSaleController(http.Controller):
         for res in env:
             _logger.error("Cliente Cordenadas {} Mi Cordenadas {}".format((latitude,longitude),(res.customer_id.partner_latitude, res.customer_id.partner_longitude)))
             if res.customer_id.partner_longitude != 0:
-                dir = gmaps.directions((latitude,longitude),
-                                       (res.customer_id.partner_latitude, res.customer_id.partner_longitude),
-                                       mode="driving",
-                                       departure_time=now)
+                dir = gmaps.reverse_geocode((latitude,longitude))
                 distance.append(dir)
             if self.compare_list(res.mapped('mobile_lines').mapped('product_id').mapped('id'),
                                  [stock['Product_id'] for stock in stock_array]):
