c3d543187c186fd724ec1d40a098e22ec45c0a14
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-22 12:56:00 -0300

    test

--- venta_movil/controllers/MobileSaleController.py
+++ venta_movil/controllers/MobileSaleController.py
@@ -249,7 +249,9 @@ class MobileSaleController(http.Controller):
 
     def round_distance(self, value):
         value_separate = str(value).split('.')
+        logging.getLogger().error(value_separate[-1])
         if int(value_separate[-1]) >= 5:
+
             return math.ceil(value)
         else:
             return round(value, 1)
