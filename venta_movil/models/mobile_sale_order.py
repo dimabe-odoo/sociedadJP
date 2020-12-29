c682251054b7069dc6d629ca7846d15b92437cc1
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-29 17:11:55 -0300

    test

--- venta_movil/models/mobile_sale_order.py
+++ venta_movil/models/mobile_sale_order.py
@@ -250,6 +250,7 @@ class MobileSaleOrder(models.Model):
             stock.write({
                 'qty_done': self.mobile_lines.filtered(lambda a: a.product_id.id == stock.product_id.id).qty,
                 'location_id':self.location_id.id,
+                'move_id':self.move_ids_without_package.filtered(lambda a: a.product_id == stock.product_id.id).id
             })
         models._logger.error('{}'.format(sale_odoo.picking_ids))
         # if self.mobile_lines.filtered(lambda a: a.loan_qty > 0):
