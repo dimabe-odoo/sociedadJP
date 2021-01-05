c3172345cc40ec21e25bc3f772a0411062dcaedb
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-05 11:12:41 -0300

    test

--- venta_movil/controllers/LoginController.py
+++ venta_movil/controllers/LoginController.py
@@ -59,7 +59,7 @@ class LoginController(http.Controller):
         truck = truck.strip()
         truck_location = request.env['stock.location'].sudo().search([('name', '=', truck)])
         session = request.env['truck.session'].sudo().search([('truck_id.id', '=', truck_location.id)])
-        if session.is_login:
+        if session.mapped('is_login'):
             return "Ya existe una sesion activa con el camion {}".format(truck)
         if truck_location:
             if not employee:
