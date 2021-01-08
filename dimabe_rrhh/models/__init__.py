9183a1b25231619e1deaf756d9e420ae18b6b928
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-08 12:23:01 -0300

    add new module

--- dimabe_rrhh/models/__init__.py
+++ dimabe_rrhh/models/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from . import models
+from . import custom_data_type
+from . import custom_data
+from . import custom_fired
+from . import custom_holiday
+from . import custom_settlement
