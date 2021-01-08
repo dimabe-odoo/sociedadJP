0f63c10e803897d68396cf1ba59e3c6cedb9575a
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-08 18:07:04 -0300

    test

--- dimabe_rrhh/__manifest__.py
+++ dimabe_rrhh/__manifest__.py
@@ -27,7 +27,8 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/custom_data.xml'
+        'views/custom_data.xml',
+        'views/custom_benefits_rrhh.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
