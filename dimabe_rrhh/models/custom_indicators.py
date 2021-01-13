f7f56dc2c612d0f9fff1cc08cad28c2891eb5584
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-13 17:52:43 -0300

    test

--- dimabe_rrhh/models/custom_indicators.py
+++ dimabe_rrhh/models/custom_indicators.py
@@ -1,5 +1,6 @@
 from odoo import fields, models, api
 import requests
+import bs4 as bs4
 
 class CustomIndicators(models.Model):
     _name = 'custom.indicators'
@@ -9,4 +10,4 @@ class CustomIndicators(models.Model):
     def get_data(self):
         link = 'https://www.previred.com/web/previred/indicadores-previsionales'
         data = requests.get(link)
-        raise models.ValidationError(data.text)
+        raise models.ValidationError(bs4.BeautifulSoup(data.text))
