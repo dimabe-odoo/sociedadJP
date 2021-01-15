7c8326ea773a759c02e0ac0dd189a20f5e161cea
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-15 16:56:31 -0300

    test

--- dimabe_rrhh/models/hr_employee.py
+++ dimabe_rrhh/models/hr_employee.py
@@ -1,6 +1,7 @@
-from odoo import models,fields
+from odoo import models, fields
+
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    employee_type_id = fields.Many2one('custom.data','Tipo de Empleado')
+    employee_type_id = fields.Many2one('custom.data', 'Tipo de Empleado', domain=[('data_type_id', '=', 7)])
---
+++ dimabe_rrhh/models/hr_payslip.py
@@ -0,0 +1,5 @@
+from odoo import models, fields
+
+
+class HrPaySlip(models.Model):
+    _inherit = 'hr.payslip'
