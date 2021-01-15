fb74644a4779506a2e1ed0b5f22803e2da73f383
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-15 09:34:18 -0300

    test

--- dimabe_rrhh/models/hr_contract.py
+++ dimabe_rrhh/models/hr_contract.py
@@ -9,3 +9,8 @@ class HrContract(models.Model):
     isapre_id = fields.Many2one('custom.isapre', 'Isapre')
 
     apv_id = fields.Many2one('custom.data', domain=[('data_type_id', '=', 1)])
+
+    currency_isapre_id = fields.Many2one('res.currency', 'Moneda', domain=[('id', 'in', (171, 173))])
+
+    is_pensionary = fields.Boolean('Pensionado')
+
