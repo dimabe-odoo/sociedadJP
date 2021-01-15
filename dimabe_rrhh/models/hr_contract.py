899fa6d306fcae1a9b8fb50c4be1bfb73e540e60
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-15 11:40:37 -0300

    test

--- dimabe_rrhh/models/hr_contract.py
+++ dimabe_rrhh/models/hr_contract.py
@@ -6,9 +6,13 @@ class HrContract(models.Model):
 
     afp_id = fields.Many2one('custom.afp', 'AFP')
 
+    apv_id = fields.Many2one('custom.data', 'Insticion APV', domain=[('data_type_id', '=', 1)])
+
+    is_fonasa = fields.Boolean('Es Fonasa')
+
     isapre_id = fields.Many2one('custom.isapre', 'Isapre')
 
-    frame_id = fields.Many2one('custom.data','Tramo',domain=[('data_type_id','=',6)])
+    frame_id = fields.Many2one('custom.data', 'Tramo', domain=[('data_type_id', '=', 6)])
 
     simple_charge = fields.Integer('Carga Simple')
 
@@ -16,7 +20,25 @@ class HrContract(models.Model):
 
     disability_charge = fields.Integer('Carga Invalidez')
 
-    apv_id = fields.Many2one('custom.data','APV', domain=[('data_type_id', '=', 1)])
+    apv_id = fields.Many2one('custom.data', 'APV', domain=[('data_type_id', '=', 1)])
+
+    isapre_agreed_quotes_uf = fields.Float('Cotizacion Pactada')
+
+    fun_number = fields.Integer('Numero FUN')
+
+    own_account_isapre = fields.Boolean('Cuenta propia isapre')
+
+    supplementary_insurance_id = fields.Many2one('custom.data', domain=['data_type_id', '=', 4])
+
+    not_afp = fields.Boolean('No Cotiza AFP')
+
+    not_afp_sis = fields.Boolean('No Cotiza AFP SIS')
+
+    saving_ccaf = fields.Float('Ahorro CCAF')
+
+    currency_supplementary_insurance_id = fields.Many2one('res.currency', 'Moneda', domain=[('id', 'in', (171, 173))])
+
+    supplementary_insurance_agreed_quotes_uf = fields.Float('Cotizacion Pactada')
 
     currency_isapre_id = fields.Many2one('res.currency', 'Moneda', domain=[('id', 'in', (171, 173))])
 
