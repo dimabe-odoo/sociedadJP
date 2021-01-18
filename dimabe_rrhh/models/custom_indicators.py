300d2581a92c5206e9802fec734a6493ad63701c
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-18 10:26:44 -0300

    test

--- dimabe_rrhh/models/custom_indicators.py
+++ dimabe_rrhh/models/custom_indicators.py
@@ -21,7 +21,7 @@ class CustomIndicators(models.Model):
 
     @api.model
     def create(self,vals):
-        raise models.ValidationError(self.month)
+        raise models.ValidationError(f'Keys {vals.keys()} Values {vals.values()}')
 
     def get_data(self):
         indicators = self.get_data_from_url()
