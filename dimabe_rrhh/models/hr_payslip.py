d13037e78a2edc896dd8e177a3ac6b954ed01f25
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-18 15:22:59 -0300

    test

--- dimabe_rrhh/models/hr_payslip.py
+++ dimabe_rrhh/models/hr_payslip.py
@@ -5,3 +5,6 @@ class HrPaySlip(models.Model):
     _inherit = 'hr.payslip'
 
     indicator_id = fields.Many2one('custom.indicators', string='Indicadores')
+
+    def compute_sheet(self):
+        raise models.ValidationError(self.indicator_id.mapped('data_ids').filtered(lambda a: a.type == '1' and not a.last_month).value)
