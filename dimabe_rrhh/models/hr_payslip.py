fdcd5d81f3534019dda9c997ca9f3847d75744d2
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-19 12:28:16 -0300

    test

--- dimabe_rrhh/models/hr_payslip.py
+++ dimabe_rrhh/models/hr_payslip.py
@@ -7,3 +7,14 @@ class HrPaySlip(models.Model):
     indicator_id = fields.Many2one('custom.indicators', string='Indicadores')
 
     salary_id = fields.Many2one('hr.salary.rule', 'Agregar Entrada')
+
+    def add(self):
+        for item in self:
+            if item.salary_id:
+                self.env['hr.payslip.input'].create({
+                    'name': item.salary_id.name,
+                    'code': item.salary_id.code,
+                    'contract_id': item.contract_id.id,
+                    'payslip_id': item.id
+                })
+            item.salary_id = None
