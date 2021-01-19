1821711b5f10be1daf2ee8227442c5390f6aaad2
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-19 12:02:51 -0300

    test

--- dimabe_rrhh/models/hr_payslip.py
+++ dimabe_rrhh/models/hr_payslip.py
@@ -5,6 +5,3 @@ class HrPaySlip(models.Model):
     _inherit = 'hr.payslip'
 
     indicator_id = fields.Many2one('custom.indicators', string='Indicadores')
-
-    def compute_sheet(self):
-        raise modes
