d9fcea7f44d55fd3b0553e8048b63566058c1f7b
dimabe-odoo <desarrolloti@dimabe.cl>
2021-01-20 10:27:32 -0300

    test

--- dimabe_rrhh/__manifest__.py
+++ dimabe_rrhh/__manifest__.py
@@ -27,7 +27,7 @@
         'data/custom_data_demo.xml',
         'data/custom_data_apv.xml',
         'data/custom_data_afp.xml',
-        'data/custom_data_hr_payslip.xml',
+        #'data/custom_data_hr_payslip.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
