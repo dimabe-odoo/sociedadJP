00012e97fd5ce25eb892926562910db12c2ff824
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-17 11:36:48 -0300

    verify button validate

--- venta_movil/controllers/ResPartnerController.py
+++ venta_movil/controllers/ResPartnerController.py
@@ -67,7 +67,7 @@ class ResPartnerController(http.Controller):
                 'Product_Name': coupon.product_tmpl_id.name,
                 'isCat': True if 'Catalítico' in coupon.product_tmpl_id.display_name else False,
                 'is_Dist': True if 'Descuento' in coupon.product_tmpl_id.display_name or 'Discount' in coupon.product_tmpl_id.display_name else False,
-                'Stock': 200,
+                'Stock': 1,
                 'Price': coupon.list_price
             })
         return result
