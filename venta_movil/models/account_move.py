14be8e11de9175e9415b68601783be3e8788074c
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-24 12:31:08 -0300

    test

--- venta_movil/models/account_move.py
+++ venta_movil/models/account_move.py
@@ -13,7 +13,6 @@ class AccountMove(models.Model):
                         'customer_id': values['partner_id'],
                         'date_discount': self.datetime.now()
                     })
-        
         return super(AccountMove, self).create(values)
 
 
