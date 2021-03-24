b8dbdea4200aa3f34dee383ea61740ed7f6a58bd
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-24 13:17:17 -0300

    test

--- venta_movil/models/account_move.py
+++ venta_movil/models/account_move.py
@@ -4,16 +4,28 @@ import datetime
 class AccountMove(models.Model):
     _inherit ='account.move'
 
-    def create(self, values):
-        for value in values:
-            if value['invoice_origin']:
-                sale_order = self.env['sale.order'].search([('name','=',value['invoice_origin'])])
-                if sale_order:
-                    discount_history = self.env['custom.discount.history'].create({
-                        'sale_id': sale_order.id,
-                        'customer_id': value['partner_id'],
-                        'date_discount': datetime.datetime.now()
-                    })
-        return super(AccountMove, self).create(values)
+    #def create(self, values):
+    #    for value in values:
+    #        if value['invoice_origin']:
+    #            sale_order = self.env['sale.order'].search([('name','=',value['invoice_origin'])])
+    #            if sale_order:
+    #                discount_history = self.env['custom.discount.history'].create({
+    #                    'sale_id': sale_order.id,
+    #                    'customer_id': value['partner_id'],
+    #                    'date_discount': datetime.datetime.now()
+    #                })
+    #    return super(AccountMove, self).create(values)
+    
+    #def write(self, values):
+    #    for value in values:
+    #        if value['invoice_origin']:
+    #            sale_order = self.env['sale.order'].search([('name','=',value['invoice_origin'])])
+    #            if sale_order:
+    #                discount_history = self.env['custom.discount.history'].create({
+    #                    'sale_id': sale_order.id,
+    #                    'customer_id': value['partner_id'],
+    #                    'date_discount': datetime.datetime.now()
+    #                })
+    #    return super(AccountMove, self).write(values)
 
 
