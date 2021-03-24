d9bec6aad929959278735f814ee020e1afa542c0
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-24 12:03:14 -0300

    test

---
+++ venta_movil/models/account_move.py
@@ -0,0 +1,14 @@
+from odoo import models
+
+class AccountMove(models.model):
+    _inherit ='account.move'
+
+    def create(self, values):
+        raise models.ValidationError(values.keys())
+        discount_history = self.env['custom.discount.history'].create({
+            'sale_id': values['sale_id'],
+            'customer_id': values['partner_id'],
+            'date_discount': self.datetime.now()
+        })
+
+        return super(AccountMove, self).create(values)
