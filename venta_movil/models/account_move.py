eff68c83daa435c7464699d0a69e3100c900e2d3
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-24 12:25:00 -0300

    test

--- venta_movil/models/account_move.py
+++ venta_movil/models/account_move.py
@@ -4,11 +4,15 @@ class AccountMove(models.Model):
     _inherit ='account.move'
 
     def create(self, values):
-        raise models.ValidationError(values[0].values())
         for value in values:
+            if value['invoice_origin']:
+                sale_order = self.env['sale_order'].search([('name','=',value['invoice_origin'])])
             discount_history = self.env['custom.discount.history'].create({
-                'sale_id': values['sale_id'],
+                'sale_id': sale_order.id,
                 'customer_id': values['partner_id'],
                 'date_discount': self.datetime.now()
             })
-            return super(AccountMove, self).create(values)
+        
+        return super(AccountMove, self).create(values)
+
+
