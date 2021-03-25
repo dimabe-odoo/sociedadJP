5f6c1da3d43a360b15ff0ae2485b3dd4ab9a7af3
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-25 11:04:08 -0300

    test

--- venta_movil/models/account_move.py
+++ venta_movil/models/account_move.py
@@ -9,13 +9,18 @@ class AccountMove(models.Model):
             if values['state'] == 'posted':
                     sale_order = self.env['sale.order'].search([('id','=',self.invoice_line_ids[0].sale_line_ids[0].order_id.id)])
                     if sale_order:
-                        if sale_order.id not in self.env['custom.discount.history'].mapped('sale_id').mapped('id'):
-                            discount_history = self.env['custom.discount.history'].create({
-                                'sale_id': sale_order.id,
-                                'customer_id': sale_order.partner_id.id,
-                                'date_discount': datetime.datetime.now(),
-                                'discount_state': 'Por Cobrar'
-                    })
+                        have_discount = False
+                        for item in sale_order.order_line:
+                            if item.product_id.cat_id.id == 7:
+                                have_discount = True
+                        if have_discount:
+                            if sale_order.id not in self.env['custom.discount.history'].mapped('sale_id').mapped('id'):
+                                discount_history = self.env['custom.discount.history'].create({
+                                    'sale_id': sale_order.id,
+                                    'customer_id': sale_order.partner_id.id,
+                                    'date_discount': datetime.datetime.now(),
+                                    'discount_state': 'Por Cobrar'
+                            })
         return super(AccountMove, self).write(values)
 
 
