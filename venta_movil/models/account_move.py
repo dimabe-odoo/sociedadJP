2c63466a820e6a3750bee199ecda9b5a10accf6c
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-25 10:02:45 -0300

    test

--- venta_movil/models/account_move.py
+++ venta_movil/models/account_move.py
@@ -10,14 +10,14 @@ class AccountMove(models.Model):
                 if values['invoice_origin']:
                     sale_order = self.env['sale.order'].search([('name','=',values['invoice_origin'])])
 
-        if sale_order:
-            if sale_order.id not in self.env['custom.discount.history'].mapped('sale_id'):
-                discount_history = self.env['custom.discount.history'].create({
-                    'sale_id': sale_order.id,
-                    'customer_id': values['partner_id'],
-                    'date_discount': datetime.datetime.now(),
-                    'discount_state': 'Por Cobrar'
-        })
+                    if sale_order:
+                        if sale_order.id not in self.env['custom.discount.history'].mapped('sale_id'):
+                            discount_history = self.env['custom.discount.history'].create({
+                                'sale_id': sale_order.id,
+                                'customer_id': values['partner_id'],
+                                'date_discount': datetime.datetime.now(),
+                                'discount_state': 'Por Cobrar'
+                    })
         return super(AccountMove, self).write(values)
 
 
