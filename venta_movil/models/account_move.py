5a76f2e9511ec45cfdf6fc93d96cdaa57f48ccb7
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-25 09:02:57 -0300

    test

--- venta_movil/models/account_move.py
+++ venta_movil/models/account_move.py
@@ -4,30 +4,22 @@ import datetime
 class AccountMove(models.Model):
     _inherit ='account.move'
 
-    #def create(self, values):
-    #    for value in values:
-    #        if value['invoice_origin']:
-    #            sale_order = self.env['sale.order'].search([('name','=',value['invoice_origin'])])
-    #            if sale_order:
-    #                discount_history = self.env['custom.discount.history'].create({
-    #                    'sale_id': sale_order.id,
-    #                    'customer_id': value['partner_id'],
-    #                    'date_discount': datetime.datetime.now()
-    #                })
-    #    return super(AccountMove, self).create(values)
-    
-    def write(self, values):
-        raise models.ValidationError(values.values())
-        if 'invoice_payment_state' in values.keys():
-            raise models.ValidationError(values['invoice_payment_state'])
-            if values['invoice_payment_state'] == 'paid':
-                sale_order = self.env['sale.order'].search([('name','=',values['invoice_origin'])])
-                if sale_order:
-                    discount_history = self.env['custom.discount.history'].create({
-                        'sale_id': sale_order.id,
-                        'customer_id': values['partner_id'],
-                        'date_discount': datetime.datetime.now()
-                })
-        return super(AccountMove, self).write(values)
+    def _compute_amount(self):
+        ids = ''
+        for item in self:
+            ids += item.id + ' '
+        raise models.ValidationError(ids)
+        #sale_order = self.env['sale.order'].search([('id','=',self.)])
+
+        #if sale_order:
+        #    discount_history = self.env['custom.discount.history'].create({
+        #        'sale_id': sale_order.id,
+        #        'customer_id': values['partner_id'],
+        #        'date_discount': datetime.datetime.now()
+        #})
+        return super(AccountMove, self)._compute_amount()
+
+
+
 
 
