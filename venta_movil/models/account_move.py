8695bbac40e07b30661784caeb0a502a2c1eddd6
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-25 09:17:21 -0300

    test

--- venta_movil/models/account_move.py
+++ venta_movil/models/account_move.py
@@ -4,20 +4,6 @@ import datetime
 class AccountMove(models.Model):
     _inherit ='account.move'
 
-    def _compute_amount(self):
-        ids = ''
-        for item in self:
-            ids += item.id + ' '
-        raise models.ValidationError(ids)
-        #sale_order = self.env['sale.order'].search([('id','=',self.)])
-
-        #if sale_order:
-        #    discount_history = self.env['custom.discount.history'].create({
-        #        'sale_id': sale_order.id,
-        #        'customer_id': values['partner_id'],
-        #        'date_discount': datetime.datetime.now()
-        #})
-        return super(AccountMove, self)._compute_amount()
 
 
 
