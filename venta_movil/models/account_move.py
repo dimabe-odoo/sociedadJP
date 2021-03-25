43f924b962ae7b5b4e7f13e3e4ebd1935ac9b906
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-25 10:44:37 -0300

    test

--- venta_movil/models/account_move.py
+++ venta_movil/models/account_move.py
@@ -8,9 +8,8 @@ class AccountMove(models.Model):
         if 'state' in values.keys():
             if values['state'] == 'posted':
                     sale_order = self.env['sale.order'].search([('id','=',self.invoice_line_ids[0].sale_line_ids[0].order_id.id)])
-                    raise models.ValidationError(sale_order.name)
                     if sale_order:
-                        if sale_order.id not in self.env['custom.discount.history'].mapped('sale_id'):
+                        if sale_order.id not in self.env['custom.discount.history'].mapped('sale_id').mapped('id'):
                             discount_history = self.env['custom.discount.history'].create({
                                 'sale_id': sale_order.id,
                                 'customer_id': values['partner_id'],
