16b40860fd54d59a8b1b40a56fab0b25f9680b5f
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-01 17:37:31 -0300

    add fields in hr contract

--- dimabe_rrhh/models/hr_contract.py
+++ dimabe_rrhh/models/hr_contract.py
@@ -6,7 +6,7 @@ class HrContract(models.Model):
 
     afp_id = fields.Many2one('custom.afp', 'AFP')
 
-    apv_id = fields.Many2one('custom.data', 'Insticion APV', domain=[('data_type_id', '=', 1)])
+    apv_id = fields.Many2one('custom.data', 'Institución APV', domain=[('data_type_id', '=', 1)])
 
     is_fonasa = fields.Boolean('Es Fonasa')
 
@@ -24,7 +24,7 @@ class HrContract(models.Model):
 
     fun_number = fields.Integer('Numero FUN')
 
-    own_account_isapre = fields.Boolean('Cuenta propia isapre')
+    own_account_isapre = fields.Boolean('Cuenta propia Isapre')
 
     supplementary_insurance_id = fields.Many2one('custom.data',string='Seguro Complementario', domain=[('data_type_id', '=', 4)])
 
@@ -47,7 +47,21 @@ class HrContract(models.Model):
     type_id = fields.Many2one('custom.data', 'Tipo de Contrato', domain=[('data_type_id', '=', 8)])
 
     type_pensionary = fields.Selection(
-        [('old', 'Pensíon de Vejez'), ('disability', 'Pension de Invalidez'), ('survival', 'Pension de Sobreviviencia'),
+        [('old', 'Pensión de Vejez'), ('disability', 'Pensión de Invalidez'), ('survival', 'Pensión de Sobreviviencia'),
          ('no', 'No Pensionado')]
-        , default='no', string='Tipo de Pension')
+        , default='no', string='Tipo de Pensión')
+
+    collation_amount = fields.Float('Asig. Colación')
+
+    mobilization_amount = fields.Float('Asig. Movilización')
+
+    viatic_amount = fields.Float('Asig. Viático')
+
+    advance_salary_amount = fields.Float('Anticipo de Sueldo')
+
+    legal_gratification = fields.Boolean('Gratificación Legal Manual')
+
+    section_id = fields.Many2one('custom.data','Viático', domain=[('data_type_id','=',6)])
+
+    
 
