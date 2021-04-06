a814cd54b98401f73709e1e683afd750340ad735
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-06 10:50:18 -0400

    test

--- dimabe_rrhh/models/hr_contract.py
+++ dimabe_rrhh/models/hr_contract.py
@@ -102,7 +102,8 @@ class HrContract(models.Model):
         sections = self.env['custom.data'].search([('data_type_id','=',self.section_type_id)])
         for section in sections:
             max_salary_section = self.env['custom.indicators.data'].search([('name','=',section.name + ' - Tope')], order='id desc')[0]
-            raise models.ValidationError(f'{len(max_salary_section)}')
+            if section.id == sections[3].id:
+                raise models.ValidationError(f'{len(max_salary_section)}')
             if max_salary_section and len(max_salary_section) > 0 and self.wage <= max_salary_section.value:
                 self.section_id = section.id
                 break
