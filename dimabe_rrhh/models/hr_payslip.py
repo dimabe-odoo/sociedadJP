cb1157d1c87e7ca5a59f1501a747bc0493527625
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-09 15:19:50 -0400

    test

--- dimabe_rrhh/models/hr_payslip.py
+++ dimabe_rrhh/models/hr_payslip.py
@@ -6,23 +6,34 @@ class HrPaySlip(models.Model):
 
     indicator_id = fields.Many2one('custom.indicators', string='Indicadores')
 
-    #salary_id = fields.Many2one('hr.salary.rule', 'Agregar Entrada')
+    salary_id = fields.Many2one('hr.salary.rule', 'Agregar Entrada')
     #salary_id = fields.Many2one('hr.payslip.input.type', 'Agregar Entrada')
-    #def add(self):
-    #    for item in self:
-    #        if item.salary_id:
-    #            type_id = self.env['hr.payslip.input.type'].search([('code','=',item.salary_id.code)])
-    #            if type_id:
-    #                self.env['hr.payslip.input'].create({
-    #                    'name': item.salary_id.name,
-    #                    'code': item.salary_id.code,
-    #                    'contract_id': item.contract_id.id,
-    #                    'payslip_id': item.id,
-    #                    'input_type_id': type_id.id
-    #                })
-    #            else:
-    #                raise models.ValidationError(f'No existe tipo {item.code}')
-    #        item.salary_id = None
+    def add(self):
+        for item in self:
+            if item.salary_id:
+                type_id = self.env['hr.payslip.input.type'].search([('code','=',item.salary_id.code)])
+                if type_id:
+                    self.env['hr.payslip.input'].create({
+                        'name': item.salary_id.name,
+                        'code': item.salary_id.code,
+                        'contract_id': item.contract_id.id,
+                        'payslip_id': item.id,
+                        'input_type_id': type_id.id
+                    })
+                else:
+                    input_type = self.env['hr.payslip.input.type'].create({
+                        'name': item.salary_id.name,
+                        'code': item.salary_id.code
+                    })
+
+                    self.env['hr.payslip.input'].create({
+                        'name': item.salary_id.name,
+                        'code': item.salary_id.code,
+                        'contract_id': item.contract_id.id,
+                        'payslip_id': item.id,
+                        'input_type_id': input_type.id
+                    })
+            item.salary_id = None
     
     @api.model
     def _get_worked_day_lines(self):
