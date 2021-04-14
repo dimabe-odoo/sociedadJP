5d83fcdc796989b9c444c35d8f2552cd15e5e9e4
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-14 13:15:11 -0400

    test

--- dimabe_rrhh/models/hr_employee.py
+++ dimabe_rrhh/models/hr_employee.py
@@ -4,12 +4,12 @@ from odoo import models, fields
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    employee_type_id = fields.Many2one('custom.data', 'Tipo de Empleado', domain=[('date_type_id.name', '=', 'Tipo de Empleado')])
+    employee_type_id = fields.Many2one('custom.data', 'Tipo de Empleado', domain=[('data_type_id.name', '=', 'Tipo de Empleado')])
 
     first_name = fields.Char("Nombre")
 
     last_name = fields.Char("Apellido")
 
-    middle_name = fields.Char("Segundo Nombre Name", help='Employees middle name')
+    middle_name = fields.Char("Segundo Nombre", help='Employees middle name')
 
     mothers_name = fields.Char("Segundo Apellido", help='Employees mothers name')
