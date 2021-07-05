a249e7f04f6cda49548b35abe068ced26554336e
dimabe-odoo <desarrolloti@dimabe.cl>
2021-07-05 16:55:27 -0400

    fix

--- dimabe_rrhh/models/custom_loan.py
+++ dimabe_rrhh/models/custom_loan.py
@@ -38,9 +38,9 @@ class CustomLoan(models.Model):
 
     fee_ids = fields.One2many('custom.fee', 'loan_id')
 
-    next_fee_id = fields.Many2one('custom.fee', compute='compute_next_fee')
+    #next_fee_id = fields.Many2one('custom.fee', compute='compute_next_fee')
 
-    next_fee_date = fields.Date('Proxima Cuota', related='next_fee_id.expiration_date')
+    #next_fee_date = fields.Date('Proxima Cuota', related='next_fee_id.expiration_date')
 
     rule_id = fields.Many2one('hr.salary.rule', string='Regla', domain=[('discount_in_fee', '=', True)],required=True)
 
