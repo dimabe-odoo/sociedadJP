915809c49eadbac89fa53efd1e5d7338aa0633fa
Fabian Droguett <desarrolloti@dimabe.cl>
2021-10-25 11:38:47 -0300

    add get_stock_product_truck

--- venta_movil/controllers/ProductController.py
+++ venta_movil/controllers/ProductController.py
@@ -3,11 +3,12 @@ from odoo.http import request
 import json
 from odoo.tools import date_utils
 
+
 class ProductController(http.Controller):
 
     @http.route('/api/get_products', type='json', method='GET', auth='token', cors='*')
-    def get_products(self,client):
-        partner = request.env['res.partner'].sudo().search([('id','=',client)])
+    def get_products(self, client):
+        partner = request.env['res.partner'].sudo().search([('id', '=', client)])
         result = []
         if partner.property_product_pricelist.item_ids:
             pricelist_id = partner.property_product_pricelist.item_ids
@@ -17,12 +18,12 @@ class ProductController(http.Controller):
                 json_data = json.dumps(raw_data, default=date_utils.json_default)
                 json_dict = json.loads(json_data)
                 if pricelist_id.filtered(lambda x: x.product_tmpl_id.id == product.product_tmpl_id.id):
-                    json_dict[0]['price'] = pricelist_id.filtered(lambda x: x.product_tmpl_id.id == product.product_tmpl_id.id).fixed_price
+                    json_dict[0]['price'] = pricelist_id.filtered(
+                        lambda x: x.product_tmpl_id.id == product.product_tmpl_id.id).fixed_price
                 else:
                     json_dict[0]['price'] = product.list_price
                 result.append(json_dict)
         return result
-
 
     @http.route('/api/get_product_truck', type='json', method='GET', auth='token', cors='*')
     def get_product_truck(self):
@@ -36,3 +37,15 @@ class ProductController(http.Controller):
                 'ImageBase64': res.image_1920
             })
         return data
+
+    @http.route('/api/get_stock_truck', type='json', method='GET', auth='token', cors='*')
+    def get_stock_product_truck(self, truck):
+        truck_stock = request.env['stock.quant'].search([('location_id.name', '=', truck)])
+        trucks = []
+        for truck in truck_stock:
+            trucks.append({
+                'ProductName': truck.product_id.name,
+                'Quantity': int(truck.quantity)
+            })
+        print(trucks)
+        return trucks
