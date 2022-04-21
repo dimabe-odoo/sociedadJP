ad35d39c999ff85e8ff3c2e1631993f21ba2e92d
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2022-04-21 09:12:02 -0400

    change

--- venta_movil/controllers/MobileSaleController.py
+++ venta_movil/controllers/MobileSaleController.py
@@ -376,7 +376,7 @@ class MobileSaleController(http.Controller):
     def get_my_order_total(self, employee):
         session = request.env['truck.session'].sudo().search([('employee_id', '=', employee)])
         orders = request.env['mobile.sale.order'].sudo().search(
-            [('seller_id', 'in', session.mapped('id')), ('state', '=', 'done')], order="date_done desc")
+            [('seller_id', 'in', session.mapped('id')), ('state', '=', 'done'),('date_done','=',datetime.datetime.now().strftime('%Y-%m-%d'))], order="date_done desc")
         order_total_money = sum(order.total_sale for order in orders)
         order_total_qty = sum(orders.mapped('mobile_lines').mapped('qty'))
 
